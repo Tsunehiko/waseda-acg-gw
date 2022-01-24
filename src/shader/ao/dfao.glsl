6dc28d01e92abd98a9214ef2c6d640696d56d09b
izumiya <k-izumiya@ruri.waseda.jp>
2022-01-24 11:13:10 +0900

    fix dfao

--- src/shader/ao/dfao.glsl
+++ src/shader/ao/dfao.glsl
@@ -10,9 +10,9 @@ const int maxIter = 18;
 float DFAO(vec3 p, vec3 n) {
     float ao = 0.0;
     for (int i = 0; i < maxIter; i++){
-        float dist = stepSize * float(i);
+        float dist = stepSize * float(i + 1);
         vec3 checkpoint = p + n * dist;
-        ao += max((dist - scene(checkpoint)) / dist, 0.0);  // TODO: i=0のとき0除算になるはず
+        ao += max((dist - scene(checkpoint)) / dist, 0.0);
     }
 
     return clamp(1.0 - ao * intensity, 0.0, 1.0);
